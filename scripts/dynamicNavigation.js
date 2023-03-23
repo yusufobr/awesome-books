d4f2de5ebcc03f17f0638e7d51bcb8f5cfa35a96
yusufobr <yousarsif80@gmail.com>
2023-03-23 15:11:21 +0000

    add styles for navigation

---
+++ scripts/dynamicNavigation.js
@@ -0,0 +1,29 @@
+const list = document.getElementById('nav-list')
+const addNew = document.getElementById('nav-add')
+const contact = document.getElementById('nav-contact')
+
+list.addEventListener('click', ()=> {
+    document.getElementById('all-books').classList.remove('hidden')
+    document.getElementById('add-new').classList.add('hidden')
+    document.getElementById('contact').classList.add('hidden')
+    list.classList.add('blue')
+    addNew.classList.remove('blue')
+    contact.classList.remove('blue')
+})
+addNew.addEventListener('click', ()=> {
+    document.getElementById('all-books').classList.add('hidden')
+    document.getElementById('add-new').classList.remove('hidden')
+    document.getElementById('contact').classList.add('hidden')
+    list.classList.remove('blue')
+    addNew.classList.add('blue')
+    contact.classList.remove('blue')
+})
+contact.addEventListener('click', ()=> {
+    document.getElementById('all-books').classList.add('hidden')
+    document.getElementById('add-new').classList.add('hidden')
+    document.getElementById('contact').classList.remove('hidden')
+    list.classList.remove('blue')
+    addNew.classList.remove('blue')
+    contact.classList.add('blue')
+})
+
